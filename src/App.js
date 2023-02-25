39f041bf12dc875539f47a47c76050e5b93d1ae1
Oxana <oavoskresenskaya@mail.ru>
2023-02-25 17:40:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useCallback, useEffect, useState, useRef } from 'react';
+import { gsap } from 'gsap';
 import './App.css';
+import image from './image.jpg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [advice, setAdvice]=useState('');
+  const header = useRef();
+  useEffect(()=>{
+    getAdvice();
+  },[])
+  const getAdvice = useCallback (async()=>{
+    const responce = await fetch ('http://www.boredapi.com/api/activity/')
+    const data = await responce.json()
+    setAdvice(data.activity)
+    gsap.from ('h2', {opacity:0, duration:2});
+  })
+return (
+    <div className='container'>
+      <img src={image} alt='foto' className='foto'/>
+      <h1>Boredom App</h1>
+      <p>If you're bored, try to pressing the button and see what you're prompted to do!</p>
+      <div ref={header}>
+        <h2>{advice}</h2>
+      </div>
+      <button onClick={getAdvice}>Change activity</button>
     </div>
   );
 }
